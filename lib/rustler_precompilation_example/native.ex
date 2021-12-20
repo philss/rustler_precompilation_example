23ef525f947fb4a4f3df809398b98a7f972b0c6e
Philip Sampaio <philip.sampaio@gmail.com>
2021-12-20 16:52:28 -0300

    Use precompilation
    This does not include a version for now because we had a problem in the
    last release. It should be fixed soon.

--- lib/rustler_precompilation_example/native.ex
+++ lib/rustler_precompilation_example/native.ex
@@ -1,5 +1,10 @@
 defmodule RustlerPrecompilationExample.Native do
-  use Rustler, otp_app: :rustler_precompilation_example, crate: "example"
+  version = Mix.Project.config()[:version]
+
+  use Rustler, otp_app: :rustler_precompilation_example, crate: "example", precompiled: [
+    base_url: "https://github.com/philss/rustler_precompilation_example/releases/download/v#{version}",
+    version: ""
+  ]
 
   # When your NIF is loaded, it will override this function.
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule RustlerPrecompilationExample.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, github: "rusterlium/rustler", sparse: "rustler_mix"}
+      {:rustler, github: "philss/rustler", branch: "ps-support-precompiled-nifs", sparse: "rustler_mix"}
     ]
   end
 end
