f0836625f382b9438c88655079701ab23411e6b5
Philip Sampaio <philip.sampaio@gmail.com>
2022-02-15 17:37:08 -0300

    Update example app with RustlerPrecompiled project
    This new project has the feature that enables the usage of precompiled
    NIFs.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Rustler Precompilation Example
 
 This is a sample project for using precompiled NIFs with Rustler.
+
+In order to use it, you either set the `RUSTLER_PRECOMPILATION_EXAMPLE_BUILD` env var
+to force compilation, or you first run the following mix task:
+
+    mix rustler_precompiled.download RustlerPrecompilationExample.Native --only-local
+
+This is going to save the checksum file locally, which is required for the project to
+load the NIF from the internet.
+
+After that, enter the console with `iex -S mix` and test the `add/2` function:
+
+```elixir
+RustlerPrecompilationExample.Native.add(2, 2)
+```
+
+For further details check the [RustlerPrecompiled project](https://github.com/philss/rustler_precompiled).
--- lib/rustler_precompilation_example/native.ex
+++ lib/rustler_precompilation_example/native.ex
@@ -1,14 +1,18 @@
 defmodule RustlerPrecompilationExample.Native do
   version = Mix.Project.config()[:version]
 
-  use Rustler,
-    otp_app: :rustler_precompilation_example,
-    crate: "example",
-    precompiled: [
+  if System.get_env("RUSTLER_PRECOMPILATION_EXAMPLE_BUILD") in ["1", "true"] do
+    use Rustler,
+      otp_app: :rustler_precompilation_example,
+      crate: "example"
+  else
+    use RustlerPrecompiled,
+      otp_app: :rustler_precompilation_example,
+      crate: "example",
       base_url:
         "https://github.com/philss/rustler_precompilation_example/releases/download/v#{version}",
       version: version
-    ]
+  end
 
   # When your NIF is loaded, it will override this function.
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
