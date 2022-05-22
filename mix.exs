24bb662c833574660da8ea612f8e4f72c21e9413
Nathan Willson <willson.b.nathan@gmail.com>
2022-05-22 16:20:30 +0900

    feat: add example package configuration with checksum included

--- mix.exs
+++ mix.exs
@@ -2,6 +2,7 @@ defmodule RustlerPrecompilationExample.MixProject do
   use Mix.Project
 
   @version "0.4.0"
+  @source_url "https://github.com/philss/rustler_precompilation_example"
 
   def project do
     [
@@ -9,7 +10,29 @@ defmodule RustlerPrecompilationExample.MixProject do
       version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      description: "A rustler precomplication example",
+      package: package(),
       deps: deps()
+    ]
+  end
+
+  # When publishing a library to with precompiled NIFs to Hex,
+  # is is mandatory to include a checksum file (along with other
+  # necessary files in the library).
+  #
+  # Refer to the "The release flow"
+  # in the "Precompilation guide" for more details:
+  # https://hexdocs.pm/rustler_precompiled/precompilation_guide.html#the-release-flow
+  defp package do
+    [
+      files: [
+        "lib",
+        "native",
+        "checksum-*.exs",
+        "mix.exs"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 
